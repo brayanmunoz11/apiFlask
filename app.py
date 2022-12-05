090983380037edb5996ac4c97d7ef068fb23ee85
Brayan Munoz <brayan.munoz@alignet.com>
2022-12-05 15:21:31 -0500

    predict

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from pydoc import apropos
 from flask import Flask, jsonify, request
 
+from sklearn.extrenals import joblib
+import sklearn
 
 app = Flask(__name__)
 
@@ -58,6 +60,35 @@ def deleteProducts(product_name):
             "products": products
         })
 
+
+@app.route('/predict', methods=['POST'])
+def addPredict():
+    json = request.get_json(force=True)
+    peso = json['peso']
+    talla = json['talla']
+    distrito = json['distrito']
+    hemoglobina = json['hemoglobina']
+    hbc = json['hbc']
+    imc = json['imc']
+
+    medidas = [peso,talla,distrito,hemoglobina,hbc,imc]
+
+    clf =   joblib.load('modelo.pkl')
+
+    pred = clf.predict(medidas)
+
+    msg = ""
+
+    if pred == 0:
+        msg = "No tiene anemia"
+    if pred == 1:
+        msg = "Tiene anemia"
+   
+    body=pred
+
+    
+    
+    return jsonify({"message": msg , "body": body})
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
